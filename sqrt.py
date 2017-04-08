25f8395a1ee600290d63a52d608a7ce7690271fe
Gary B. Corell <wa5qjh@xmission.com>
2017-04-08 16:21:47 +0800

    Add files via upload

--- sqrt.py
+++ sqrt.py
@@ -4,13 +4,20 @@ import sys
 
 print (math)
 print (sys)
+
 """
 sqare root function
  y = (x + a/x)/2
+ useage sqrt(integer) integer is non-negative
+ returns float
 
 """
 
 def sqrt( a ) :
+    if a <1:
+        print ("sorry, we dont do negative number or zero")
+        print ("useage sqrt( non-negative-integer ) "   )
+
     y=1.0			# force floating point
     x=a
     while x != y :
@@ -18,15 +25,17 @@ def sqrt( a ) :
         y = ( x + a/x ) / 2
     return y
 
-#    ----------    
-print ( "\n\t Testing my sqrt vs math.sqrt \n")
-tri=556
-print (sys.version_info)
-if sys.version_info[ 0] ==  3 :
-    print ("\t\t py3 My sqrt of %d is %.5f " %  (tri, sqrt( tri ) ) )
-    print ("\t\t math.sqrt \t %.5f" % math.sqrt( tri ) )
-else:
-    print ("\t\t py2 My sqrt of %d is  %.5f " %  ( tri, sqrt( tri ) ) )
-    print ("\t\t math.sqrt \t %.5f" %  math.sqrt( tri ) )
-    
+def test_sqrt(tri) :
+    print ( "\n\t Testing my sqrt vs math.sqrt")
+    print (sys.version_info)
+    if sys.version_info[0] == 3:
+        print ("\t\t p3 My sqrt of %d is %.5f " % (tri, sqrt(tri)))
+        print ("\t\t math.sqrt \t %.5f" % math.sqrt(tri))
+    else:
+        print ("\t\t p2 My sqrt of %d is  %.5f " % (tri, sqrt(tri)))
+        print ("\t\t math.sqrt \t %.5f" % math.sqrt(tri))
+
+#    ----------
+
+test_sqrt(555)
 
