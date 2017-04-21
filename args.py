ec3df7a9b325248dadef9de5b6deb929a170c0d3
Dale E. Moore <DaleEMoore@gMail.Com>
2017-04-21 11:00:58 -0500

    args expanded

--- args.py
+++ args.py
@@ -7,3 +7,7 @@
 import sys
 
 print (sys.argv)
+
+print ("This is the name of the script: ", sys.argv[0])
+print ("Number of arguments: ", len(sys.argv))
+print ("The arguments are: " , str(sys.argv))
